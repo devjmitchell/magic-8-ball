867b5ed31efd58f1725d147f5bf6e18d68ccd0a0
semperphoenix <semperphoenixaz@gmail.com>
2017-01-23 21:19:32 -0700

    changed answer dictionary to list and added "shaking" sleep

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
-from random import randint
+from random import choice
+from time import sleep
 
 # Welcome message
 print ("\nWelcome to Magic 8 Ball!")
@@ -6,58 +7,56 @@ print ("\nWelcome to Magic 8 Ball!")
 # Start "keep_playing" flag as True
 keep_playing = True
 
-# Dictionary of answers based on random_number
-answer = {
-    1: "\n*** It is certain ***",
-    2: "\n*** It is decidedly so ***",
-    3: "\n*** Without a doubt ***",
-    4: "\n*** Yes, definitely ***",
-    5: "\n*** You may rely on it ***",
-    6: "\n*** As I see it, yes ***",
-    7: "\n*** Most likely ***",
-    8: "\n*** Outlook good ***",
-    9: "\n*** Yes ***",
-    10: "\n*** Signs point to yes ***",
-    11: "\n*** Reply hazy try again ***",
-    12: "\n*** Ask again later ***",
-    13: "\n*** Better not tell you now ***",
-    14: "\n*** Cannot predict now ***",
-    15: "\n*** Concentrate and ask again ***",
-    16: "\n*** Don't count on it ***",
-    17: "\n*** My reply is no ***",
-    18: "\n*** My sources say no ***",
-    19: "\n*** Outlook not so good ***",
-    20: "\n*** Very doubtful ***",
-    21: "\n*** No ***",
-    # The following are funny answers, adjust random_number range to use / not use
-    22: "\n*** HELP ME, I'M TRAPPED IN THIS 8 BALL! ***",
-    23: "\n*** Warning, I don't know what I'm talking about! ***",
-    24: "\n*** Why are you asking me? I just give you random replies back! ***",
-    25: "\n*** Shake and ask again... Don't actually shake, this is a computer! ***",
-    26: "\n*** Just Google it! ***",
-    27: "\n*** I dunno ***",
-    28: "\n*** Just get lost! ***",
-    29: "\n*** Please stop asking questions, you've asked enough! ***"
-}
+# List of answers
+answer = [
+    "*** It is certain ***",
+    "*** It is decidedly so ***",
+    "*** Without a doubt ***",
+    "*** Yes, definitely ***",
+    "*** You may rely on it ***",
+    "*** As I see it, yes ***",
+    "*** Most likely ***",
+    "*** Outlook good ***",
+    "*** Yes ***",
+    "*** Signs point to yes ***",
+    "*** Reply hazy try again ***",
+    "*** Ask again later ***",
+    "*** Better not tell you now ***",
+    "*** Cannot predict now ***",
+    "*** Concentrate and ask again ***",
+    "*** Don't count on it ***",
+    "*** My reply is no ***",
+    "*** My sources say no ***",
+    "*** Outlook not so good ***",
+    "*** Very doubtful ***",
+    "*** No ***",
+    # The following are funny answers, comment them out if don't want to use
+    "*** HELP ME, I'M TRAPPED IN THIS 8 BALL! ***",
+    "*** Warning, I don't know what I'm talking about! ***",
+    "*** Why are you asking me? I just give you random replies back! ***",
+    "*** Shake and ask again... Don't actually shake, this is a computer! ***",
+    "*** Just Google it! ***",
+    "*** I dunno ***",
+    "*** Just get lost! ***",
+    "*** Please stop asking questions, you've asked enough! ***"
+]
 
 # The game
 while keep_playing:
-    # Ask for a question
+    # Ask for a question and verify not blank
     question = input("\nAsk a question: ")
-
-    # If question is blank, ask again
     while question == "":
         print ("\nYou didn't ask anything")
         question = input("Ask a question: ")
 
-    # Get a random number that will be used for the magic answer
-    random_number = randint(1,29)
+    # Shake, then print a random answer from answer list
+    print("\nShaking...")
+    sleep(2)
+    print (choice(answer))
 
-    # Call dictionary value to give magic answer
-    print (answer[random_number])
-
-    # Ask if user would like to play again
-    play_again = input("Would you like to ask another question? (y/n) ")
+    # Pause 3 seconds, then ask if user would like to play again
+    sleep(3)
+    play_again = input("\nWould you like to ask another question? (y/n) ")
 
     # Change "keep_playing" flag to end play if user didn't say "y" or "yes"
     if play_again.lower() != "y" and play_again.lower() != "yes":
